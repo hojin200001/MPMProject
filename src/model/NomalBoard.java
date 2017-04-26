e9a42307097f6546745a37e385173d5bf0563c6f
hojin200001 <asxzqwsaerfd@naver.com>
2017-04-26 18:00:16 +0900

    수정 커밋

--- src/model/NomalBoard.java
+++ src/model/NomalBoard.java
@@ -1,97 +1,4 @@
-<<<<<<< HEAD
 
-package model;
-
-import java.util.Date;
-
-public class NomalBoard {
-	private int nnum;
-	private String ntitle;
-	private String narea;
-	private String njob;
-	private String ncareer;
-	private String nphone;
-	private String nemail;
-	private String nday;
-	private Date njoinDay;
-	private int ncount;
-	private String nomalId;
-	
-	public int getNnum() {
-		return nnum;
-	}
-	public void setNnum(int nnum) {
-		this.nnum = nnum;
-	}
-	public String getNtitle() {
-		return ntitle;
-	}
-	public void setNtitle(String ntitle) {
-		this.ntitle = ntitle;
-	}
-	public String getNarea() {
-		return narea;
-	}
-	public void setNarea(String narea) {
-		this.narea = narea;
-	}
-	public String getNjob() {
-		return njob;
-	}
-	public void setNjob(String njob) {
-		this.njob = njob;
-	}
-	public String getNcareer() {
-		return ncareer;
-	}
-	public void setNcareer(String ncareer) {
-		this.ncareer = ncareer;
-	}
-	public String getNphone() {
-		return nphone;
-	}
-	public void setNphone(String nphone) {
-		this.nphone = nphone;
-	}
-	public String getNemail() {
-		return nemail;
-	}
-	public void setNemail(String nemail) {
-		this.nemail = nemail;
-	}
-	public String getNday() {
-		return nday;
-	}
-	public void setNday(String nday) {
-		this.nday = nday;
-	}
-	public Date getNjoinDay() {
-		return njoinDay;
-	}
-	public void setNjoinDay(Date njoinDay) {
-		this.njoinDay = njoinDay;
-	}
-	public int getNcount() {
-		return ncount;
-	}
-	public void setNcount(int ncount) {
-		this.ncount = ncount;
-	}
-	public String getNomalId() {
-		return nomalId;
-	}
-	public void setNomalId(String nomalId) {
-		this.nomalId = nomalId;
-	}
-	
-	@Override
-	public String toString() {
-		return "NomalBoard [nnum=" + nnum + ", ntitle=" + ntitle + ", narea=" + narea + ", njob=" + njob + ", ncareer="
-				+ ncareer + ", nphone=" + nphone + ", nemail=" + nemail + ", nday=" + nday + ", njoinDay=" + njoinDay
-				+ ", ncount=" + ncount + ", nomalId=" + nomalId + "]";
-	}
-}
-=======
 package model;
 
 import java.util.Date;
@@ -186,8 +93,6 @@ public class NomalBoard {
 		return "NomalBoard [nnum=" + nnum + ", ntitle=" + ntitle + ", narea=" + narea + ", njob=" + njob + ", ncareer="
 				+ ncareer + ", nphone=" + nphone + ", nemail=" + nemail + ", nday=" + nday + ", njoinDay=" + njoinDay
 				+ ", ntext=" + ntext + ", ncount=" + ncount + ", nomalId=" + nomalId + "]";
-	}
-	
-	
+	}	
 }
->>>>>>> 221921f4d18ad5f055177ca0e7dccde4d63e2a7c
+
