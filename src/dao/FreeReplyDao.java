68028b3526f333cb8c04d614ba1c99d7e5f22afe
GaratoDJ <garato@naver.com>
2017-05-12 09:28:57 +0900

    ㅇㅅㅇ?

--- src/dao/FreeReplyDao.java
+++ src/dao/FreeReplyDao.java
@@ -9,9 +9,6 @@ public interface FreeReplyDao {
 	public void insertFreeReply(FreeReplyVo freeReplyVo);
 	public void updateFreeReply(FreeReplyVo freeReplyVo);
 	public void deleteFreeReply(int ReNum);
-<<<<<<< HEAD
 	public List<FreeReplyVo> selectAll();
-=======
 	public List<FreeReplyVo> selectReply(int num);
->>>>>>> cdf2d34813135f8ea4b5d997b7e7bb181173b1e4
 }
